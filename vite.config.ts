9cd5230a04a2ff433866c52899b869bd0b40203f
Balaraju24 <balaraju.p@orotron.com>
2025-09-19 21:49:47 +0530

    Update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "TanstackTableComponent",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "cjs"], // make sure both get built
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "@tanstack/react-table"],
+    },
+  },
 });
